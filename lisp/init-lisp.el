a07af3589811920c53b8ef79efcfa3e370f00b5d
gaowei <gaowei@zrcaifu.com>
2015-11-11 14:51:24 +0800

    update

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,11 +1,12 @@
 (require-package 'elisp-slime-nav)
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "ELisp")))
 
 (require-package 'lively)
 
 (setq-default initial-scratch-message
-              (concat ";; Happy hacking " (or user-login-name "") " - Emacs ♥ you!\n\n"))
+              (concat ";; Happy hacking " user-login-name " - Emacs ♥ you!\n\n"))
 
 
 
