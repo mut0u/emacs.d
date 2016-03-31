cd479a05e65a7e4f9202d1f824c888daf8b356ef
gaowei <gaowei@zrcaifu.com>
2016-03-31 13:40:38 +0800

    update

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -23,4 +23,28 @@
   (after-load 'dired
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
 
+
+(require-package 'swiper)
+(require 'cl)
+(require 'ivy)
+
+
+(defun goto-recent-directory ()
+  "Recent directories"
+  (interactive)
+  (let (collection)
+    (unless recentf-mode (recentf-mode 1))
+    (setq collection
+          (append (remove-duplicates
+                   (mapcar 'file-name-directory recentf-list)
+                   :test (lambda (x y) (or (null y) (equal x y)))
+                   :from-end t)
+                  ;; fasd history
+                  (if (executable-find "fasd")
+                      (mapcar (lambda (v) (replace-regexp-in-string "^[.0-9]+ +" "" v))
+                              (split-string (shell-command-to-string "fasd -d") "\n" t)))))
+    (ivy-read "directories:" collection :action 'dired)))
+
+
+
 (provide 'init-dired)
