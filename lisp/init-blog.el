86ff9294e1a364765ec85a63c194b36d777dfe8b
gaowei <gaowei@zrcaifu.com>
2016-10-12 17:50:36 +0800

    update

--- lisp/init-blog.el
+++ lisp/init-blog.el
@@ -2,23 +2,64 @@
 
 (require 'ox-publish)
 
-(defconst base-directory "/Users/savior/github.respo/mut0u.github.io/notes/")
-(defconst publishing-directory "~/github.respo/mut0u.github.io/public_html/")
+(defconst base-directory "")
+
+(defcustom blog-base-directory "~/org/notes/"
+  "base for blog dir"
+  :group 'org-blog
+  :type 'string)
+
+
+
+;;(defconst publishing-directory "~/github.respo/mut0u.github.io/public_html/")
+
+(defcustom blog-publishing-directory "~/org/public_html/"
+  "dir for genreate html file come from org file"
+  :group 'org-blog
+  :type 'string)
+
+
+(defcustom blog-static-directory "~/org/static/"
+  "static dir for css and js file"
+  :group 'org-blog
+  :type 'string)
+
+
+(defcustom blog-author "unknow"
+  "author"
+  :group 'org-blog
+  :type 'string)
+
+
+(defcustom blog-email "test@unknow.com"
+  "email"
+  :group 'org-blog
+  :type 'string)
+
+
+
+
+(when (not (file-exists-p blog-base-directory))
+  (make-directory blog-base-directory t))
+
+(when (not (file-exists-p blog-publishing-directory))
+  (make-directory blog-publishing-directory t))
+
 
 (setq org-publish-project-alist
-      '(
+      `(
         ;; 把各部分的配置文件写到这里面来
         ("-notes"
-         :base-directory "/Users/savior/github.respo/mut0u.github.io/notes/" ;;"~/org/"
+         :base-directory  ,blog-base-directory
          :base-extension "org"
-         :publishing-directory "~/github.respo/mut0u.github.io/" ;;"~/public_html/"
+         :publishing-directory ,blog-publishing-directory
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4             ; Just the default for this project.
          :auto-preamble t
          :section-numbers nil
-         :author "savior"
-         :email "michael.savior@gmail.com"
+         :author ,blog-author
+         :email ,blog-email
          ;;         :auto-sitemap t                ; Generate sitemap.org automagically...
          ;;         :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
          ;;         :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
@@ -27,9 +68,9 @@
          )
 
         ("-static"
-         :base-directory "/Users/savior/github.respo/mut0u.github.io/static/" ;;"~/org/"
+         :base-directory ,blog-static-directory
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory "~/github.respo/mut0u.github.io/" ;;"~/public_html/"
+         :publishing-directory ,blog-publishing-directory
          :recursive t
          :publishing-function org-publish-attachment)
         ("blog" :components ("-notes" "-static"))))
