c61109071409da249c0de60c058538f2295c3aca
gaowei <gaowei@zrcaifu.com>
2016-10-23 15:38:59 +0800

    change move dup key coflict with projectile

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -225,8 +225,8 @@
 (global-set-key [M-S-up] 'md/move-lines-up)
 (global-set-key [M-S-down] 'md/move-lines-down)
 
-(global-set-key (kbd "C-c p") 'md/duplicate-down)
-(global-set-key (kbd "C-c P") 'md/duplicate-up)
+(global-set-key (kbd "C-c d") 'md/duplicate-down)
+(global-set-key (kbd "C-c u") 'md/duplicate-up)
 
 ;;----------------------------------------------------------------------------
 ;; Fix backward-up-list to understand quotes, see http://bit.ly/h7mdIL
