aa0a3074d4bf70fcfa5ff685405ca8a39e5ad7a0
savior <michael.savior@gmail.com>
2017-06-11 15:45:41 +0800

    fix

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -19,7 +19,7 @@
 ;; haskell process, if any.
 
 (require-package 'ac-haskell-process)
-
+(custom-set-variables '(haskell-process-type 'stack-ghci))
 (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
 (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
 
