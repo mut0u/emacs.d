1940a0b926422f96f2de691c8b0007238951e143
gaowei <gaowei.gauss@bytedance.com>
2022-06-13 01:49:54 +0800

    update some config.

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -114,9 +114,8 @@ there is no current file, eval the current buffer."
 (with-eval-after-load 'ielm
   (define-key ielm-map (kbd "C-c C-z") 'sanityinc/repl-switch-back))
 
-;; ----------------------------------------------------------------------------
+
 ;; Hippie-expand
-;; ----------------------------------------------------------------------------
 
 (defun set-up-hippie-expand-for-elisp ()
   "Locally set `hippie-expand' completion functions for use with Emacs Lisp."
@@ -125,17 +124,17 @@ there is no current file, eval the current buffer."
   (add-to-list 'hippie-expand-try-functions-list 'try-complete-lisp-symbol-partially t))
 
 
-;; ----------------------------------------------------------------------------
+
 ;; Automatic byte compilation
-;; ----------------------------------------------------------------------------
+
 (when (maybe-require-package 'auto-compile)
   (setq auto-compile-delete-stray-dest nil)
   (add-hook 'after-init-hook 'auto-compile-on-save-mode)
   (add-hook 'after-init-hook 'auto-compile-on-load-mode))
 
-;; ----------------------------------------------------------------------------
+
 ;; Load .el if newer than corresponding .elc
-;; ----------------------------------------------------------------------------
+
 (setq load-prefer-newer t)
 
 
@@ -160,9 +159,9 @@ there is no current file, eval the current buffer."
        " ")))))
 
 
-;; ----------------------------------------------------------------------------
+
 ;; Enable desired features for all lisp modes
-;; ----------------------------------------------------------------------------
+
 (defun sanityinc/enable-check-parens-on-save ()
   "Run `check-parens' when the current buffer is saved."
   (add-hook 'after-save-hook #'check-parens nil t))
@@ -207,13 +206,9 @@ there is no current file, eval the current buffer."
 (add-to-list 'auto-mode-alist '("\\.emacs-project\\'" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("archive-contents\\'" . emacs-lisp-mode))
 
-(require-package 'cl-lib-highlight)
-(with-eval-after-load 'lisp-mode
-  (cl-lib-highlight-initialize))
 
-;; ----------------------------------------------------------------------------
+
 ;; Delete .elc files when reverting the .el from VC or magit
-;; ----------------------------------------------------------------------------
 
 ;; When .el files are open, we can intercept when they are modified
 ;; by VC or magit in order to remove .elc files that are likely to
@@ -275,11 +270,8 @@ there is no current file, eval the current buffer."
   (add-hook 'emacs-lisp-mode-hook 'highlight-quoted-mode))
 
 
-(when (maybe-require-package 'flycheck)
-  (require-package 'flycheck-package)
-  (with-eval-after-load 'flycheck
-    (with-eval-after-load 'elisp-mode
-      (flycheck-package-setup))))
+(when (maybe-require-package 'package-lint-flymake)
+  (add-hook 'emacs-lisp-mode-hook #'package-lint-flymake-setup))
 
 
 
